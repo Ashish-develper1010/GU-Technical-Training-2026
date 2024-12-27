5df93807ae57a31d3ed3ba7666c9c698671ccacb
Ashish-develper1010 <ac6100861@gmail.com>
2024-12-27 19:16:24 +0530

    Create problem1.java

---
+++ problem1.java
@@ -0,0 +1,8 @@
+         // 1. WAP to take input 2 numbers from the user and print the sum of the numbers.
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number ");
+        int first = sc.nextInt();
+        System.out.print("Enter second number ");
+        int second = sc.nextInt();
+        int sum = first+second;
+        System.out.println("Sum of "+first+" and "+second+" is : "+ sum);
